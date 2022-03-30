391bc446d3b501c553a9d2a4614d4d4798cf05b9
Aaron Dee <aarondee2014@gmail.com>
2022-03-30 22:43:57 +0100

    Add project files.

---
+++ ClassMate/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ClassMate.ViewModels;
+using ClassMate.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ClassMate
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ ClassMate/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ClassMate.Models;
+using ClassMate.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClassMate.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
